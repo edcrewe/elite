73dd02540988d197416d45a3d06851408e36230b
Ed Crewe <ed.crewe@oracle.com>
2018-08-12 20:22:20 +0100

    add post handler

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask
+from flask import Flask, request
 from jinja2 import Template
 app = Flask(__name__)
 
@@ -16,3 +16,18 @@ def add():
     with open('templates/add.html.j2') as tmpl:
         template = Template(tmpl.read())
     return template.render()        
+
+@app.route('/runpost', methods=['POST'])
+def runpost():
+    if request.form:
+        with open('templates/runs.json') as fh:
+            runs = json.loads(fh.read())
+        run = {}
+        run['datetime'] = request.form['date'] + ':' + request.form['time'] + '00'
+        for key in request.form:
+            if key not in ('date', 'time'):
+                run[key] = int(request.form[key])
+        runs.append(run)
+        with open('templates/runs.json', 'w') as fh:        
+            fh.write(json.dumps(runs))
+    return hello()
